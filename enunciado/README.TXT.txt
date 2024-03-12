dedf4ec9a2ff136abd6fc907c07f41ddaaee35ab
lily <lianys.herrera@hotmail.com>
2024-03-12 15:58:31 +0100

    Primera parte hecha

---
+++ enunciado/README.TXT.txt
@@ -0,0 +1,5 @@
+Os propongo una primera tarea de repaso.
+Consiste en:
+1.- al pasar le ratón por las imágenes debe salir en el párrafo que hay tras el h4 el texto: "Esta es la parada de ..." (la parada que sea)
+2.-Los botones son para seleccionar bordeando del color que corresponde las imágenes de las paradas de las 3 líneas de metro.Sabréis a qué línea pertenece la parada porque está en el atributo class de la etiqueta img
+Ejemplo: Konstanzer pertenece a la línea roja mientras que Jungfernheide pertenece a la azul y a la verde
---
+++ enunciado/js/script.js
@@ -0,0 +1,39 @@
+const rojo = document.getElementsByTagName("button")[0];
+const verde = document.getElementsByTagName("button")[1];
+const azul = document.getElementsByTagName("button")[2];
+const borrar = document.getElementsByTagName("button")[3];
+
+
+rojo.addEventListener("click", () => {
+    const imgRojo = document.querySelectorAll('.roja');
+    imgRojo.forEach(img =>{
+        if(img.tagName.toLowerCase()=== 'img') {
+            img.style.border= '3px solid red';
+        }
+    })
+});
+
+verde.addEventListener("click", () => {
+    const imgVerde = document.querySelectorAll('.verde');
+    imgVerde.forEach(imgV => {
+        if(imgV.tagName.toLowerCase()=== 'img'){
+            imgV.style.border= '3px solid green';
+        }
+    })
+});
+
+azul.addEventListener("click", () => {
+    const imgAzul = document.querySelectorAll('.azul');
+    imgAzul.forEach(imgA => {
+        if(imgA.tagName.toLowerCase()=== 'img'){
+            imgA.style.border= '3px solid blue';
+        }
+    })
+});
+
+borrar.addEventListener("click", () => {
+    const img =document.querySelectorAll('img');
+    img.forEach(imagen => {
+        imagen.style.border= 'none';
+    })
+});
